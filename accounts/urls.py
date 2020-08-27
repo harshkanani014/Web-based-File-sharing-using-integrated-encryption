5f277d9682fc17a0aef93a281b2fffc6719efcf9
harshkanani014 <harshkanani100@gmail.com>
2020-08-27 10:39:33 +0530

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("register", views.register, name="register"),
+    path("login",views.login, name="login"),
+    path("logout",views.logout,name="logout"),
+    path("", views.home_page, name="home_page")
+    ]
